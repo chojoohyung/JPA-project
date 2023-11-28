2b17b7358bece2c284eccaf0da319c0eaadcc9f0
leeinbeom <dqdq7979@naver.com>
2023-11-28 22:31:25 +0900

    병합 충돌 해결

--- WebSocketChattingServer/src/main/java/com/portfolio/WebSocketChattingServerApplication.java
+++ WebSocketChattingServer/src/main/java/com/portfolio/WebSocketChattingServerApplication.java
@@ -4,17 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-<<<<<<<< HEAD:Pro/src/main/java/com/example/demo/ProApplication.java
-public class ProApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ProApplication.class, args);
-========
 public class WebSocketChattingServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebSocketChattingServerApplication.class, args);
->>>>>>>> main:WebSocketChattingServer/src/main/java/com/portfolio/WebSocketChattingServerApplication.java
 	}
 
 }
--- WebSocketChattingServer/src/test/java/com/portfolio/WebSocketChattingServerApplicationTests.java
+++ WebSocketChattingServer/src/test/java/com/portfolio/WebSocketChattingServerApplicationTests.java
@@ -4,11 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-<<<<<<<< HEAD:Pro/src/test/java/com/example/demo/ProApplicationTests.java
-class ProApplicationTests {
-========
 class WebSocketChattingServerApplicationTests {
->>>>>>>> main:WebSocketChattingServer/src/test/java/com/portfolio/WebSocketChattingServerApplicationTests.java
 
 	@Test
 	void contextLoads() {
