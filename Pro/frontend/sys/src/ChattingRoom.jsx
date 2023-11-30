503ed33e530e40b6762fba35829a0513edf26764
leeinbeom <dqdq7979@naver.com>
2023-11-30 21:38:09 +0900

    asd

--- Pro/frontend/sys/src/ChattingRoom.jsx
+++ Pro/frontend/sys/src/ChattingRoom.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const ChattingRoom = () => {
+  useEffect(() => {
+    const sendGetRequest = async () => {
+      try {
+        const response = await fetch('http://localhost:8080');
+        if (response.ok) {
+          const data = await response.json();
+          console.log('Response from server:', data);
+        } else {
+          console.error('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    sendGetRequest();
+  }, []);
+
   return (
     <div>
       <h1>Chatting Room</h1>
       <p>Welcome to the Chatting Room!</p>
-   
+    
     </div>
   );
 };
